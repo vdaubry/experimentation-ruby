a37b0864d0134cc89f83ff2c08f99b4bf79da16e
Vincent Daubry <vdaubry@gmail.com>
2016-04-28 15:54:40 +0200

    DB cinnect in benchmark

--- Benchmark/ips.rb
+++ Benchmark/ips.rb
@@ -1,13 +1,19 @@
-require 'benchmark/ips'
-location="worldwide"
-period="all_time"
-offset=0
-count=25
+#Run in console : RAILS_ENV=production bin/rails c
 
+#we're in production mode so we load all environment vars manually
+require 'benchmark/ips'
+require 'dotenv'
+
+Dotenv.load
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+
+current_user = User.first
 Benchmark.ips do |x|
-	x.report("rankings") do |times|
-	    users_rankings = Rankings::UserCollectionRankings.new(location: location, period: period)
-    	users = users_rankings.sorted_users(offset: offset, count: count)
-    	Api::V0::RankingsSerializer.new(users: users, location: location).to_json
-  	end
+  x.report("rankings") do |times|
+    videos = Video.includes(:tags, :uploader)
+                 .not_seen_by_user(current_user)
+                 .random
+                 .limit(25)
+    Api::V0::VideoSerializer.serialize_array(video_array: videos, current_user: current_user)
+  end
 end
